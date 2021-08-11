3c456037eda46fe2f0e8428ec0a9d5df4dd7cae5
Amitojha74 <amitojha130399@gmail.com>
2021-08-11 16:09:51 +0530

    Variable name update.

--- src/main/TestApplication.java
+++ src/main/TestApplication.java
@@ -5,11 +5,11 @@ import java.util.*;
 public class TestApplication {
 
     public static void main(String[] args) {
+
         Scanner input =new Scanner(System.in);
         BookServiceImpl bookServiceImpl = new BookServiceImpl();
-        Book b1=new Book();
-        int userId,ch,ch1,c=0,id;
-        String name;
+        int userId, choice, count =0, bookId;
+        String nameForSearch;
         List<Book> book1=new ArrayList<>();
         List<Book> issue=new ArrayList<>();
         Queue<RequestBook> reqBook=new PriorityQueue<>();
@@ -23,16 +23,16 @@ public class TestApplication {
             System.out.println("Welcome to the Book App");
             System.out.println("1. For Enter into System");
             System.out.println("2. For Exit()");
-            ch1 = input.nextInt();
-            if(ch1==1) {
+            choice = input.nextInt();
+            if(choice ==1) {
                 System.out.println("Enter User Id: ");
                 userId= input.nextInt();
                 for (User user1 : user) {
                     if (userId == user1.getUserId()) {
-                        c = 1;
+                        count = 1;
                     }
                 }
-                if (c == 1) {
+                if (count == 1) {
                     while (true) {
                         System.out.println("1. For Showing All Books");
                         System.out.println("2. For Showing Issue Book List");
@@ -43,39 +43,39 @@ public class TestApplication {
                         System.out.println("7. For Requesting A Book");
                         System.out.println("8. Requesting Book List");
                         System.out.println("9. For Exit()");
-                        ch = input.nextInt();
-                        if (ch == 1) {
+                        choice = input.nextInt();
+                        if (choice == 1) {
                             System.out.println("All Book List");
                             System.out.println(book1);
-                        } else if (ch == 2) {
+                        } else if (choice == 2) {
                             System.out.println("Issue Book List");
                             System.out.println(issue);
-                        } else if (ch == 3) {
+                        } else if (choice == 3) {
                             book1= bookServiceImpl.addBook(userId);
                             System.out.println("Book Added Successfully");
                         }
-                        else if(ch==4){
+                        else if(choice==4){
                             List<Book> searchBook=new ArrayList<>();
                             System.out.println("Enter string to search book");
-                            name= input.next();
-                            searchBook= bookServiceImpl.searchBook(name);
+                            nameForSearch = input.next();
+                            searchBook= bookServiceImpl.searchBook(nameForSearch);
                             System.out.println("Search Result is:");
                             System.out.println(searchBook);
                         }
-                        else if(ch==5) {
+                        else if(choice==5) {
                             System.out.println("Enter string to search book");
-                            name = input.next();
+                            nameForSearch = input.next();
                             List<Book> searchBook = new ArrayList<>();
-                            searchBook = bookServiceImpl.searchBook(name);
+                            searchBook = bookServiceImpl.searchBook(nameForSearch);
                             if (searchBook.isEmpty()) {
                                 System.out.println("Book Not Available with Search String");
                             } else {
                                 System.out.println(searchBook);
                                 System.out.println("Enter Id to which you issue book");
-                                id = input.nextInt();
+                                int issuerId = input.nextInt();
                                 System.out.println("Enter Book Id");
-                                int bookId = input.nextInt();
-                                issue = bookServiceImpl.issueBook(bookId, id, userId, searchBook);
+                                bookId = input.nextInt();
+                                issue = bookServiceImpl.issueBook(bookId, issuerId, userId, searchBook);
                                 if (issue.isEmpty()) {
                                     System.out.println("Book Not Available or you have not authority to issue");
                                 } else {
@@ -84,18 +84,18 @@ public class TestApplication {
                                 }
                             }
                         }
-                        else if(ch==6){
+                        else if(choice==6){
                             System.out.println("Enter Book Id");
-                            id= input.nextInt();
-                            bookServiceImpl.returnBook(id,issue);
+                            bookId= input.nextInt();
+                            bookServiceImpl.returnBook(bookId,issue);
                             System.out.println("Book Returned Successfully");
                         }
-                        else if(ch==7){
+                        else if(choice==7){
                             reqBook= bookServiceImpl.requestBook(userId);
                             System.out.println("Book Is Requested");
                             System.out.println(reqBook);
                         }
-                        else if(ch==8){
+                        else if(choice==8){
                             System.out.println("Requested book List:");
                             System.out.println(reqBook);
                         }
